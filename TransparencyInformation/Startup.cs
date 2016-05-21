170217eaba19f485512292c13d21bdbfcce901d1
SuperPC <SuperPC@DESKTOP-1167R8S>
2016-05-21 09:25:16 +0700

    Add project files.

---
+++ TransparencyInformation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TransparencyInformation.Startup))]
+namespace TransparencyInformation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
